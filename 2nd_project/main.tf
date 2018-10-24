d3b0f51cb930cf10edf66761d41605de86b6706a
tomerleib <tomer.leibovich@gmail.com>
2018-10-24 20:23:56 +0300

    Included ASG, ELB and Spot

--- 2nd_project/main.tf
+++ 2nd_project/main.tf
@@ -54,21 +54,66 @@ data "aws_ami" "ubuntu" {
 
   owners = ["099720109477"] # Canonical
 }
+resource "aws_elb" "test_lb" {
+  name = "project-terraform-lb"
+  subnets = ["${var.public_subnets}"]
 
-resource "aws_instance" "myinstance" {
-    ami = "${data.aws_ami.ubuntu.id}"
+  listener {
+    instance_port = 80
+    instance_protocol = "http"
+    lb_port = 80
+    lb_protocol = "http"
+  }
+
+  health_check {
+    healthy_threshold = 3
+    unhealthy_threshold = 3
+    timeout = 10
+    target = "HTTP:80/index.html"
+    interval = 30
+  }
+
+  security_groups = ["${aws_security_group.http.id}"]
+}
+
+resource "aws_launch_configuration" "my_lc" {
+    name_prefix = "project_lc"
+    image_id = "${data.aws_ami.ubuntu.id}"
     instance_type = "t2.micro"
-    vpc_security_group_ids = ["${aws_security_group.http.id}"]
-    subnet_id = "${var.subnet_id}"
-    associate_public_ip_address = true
-    key_name = "tomer-test"
+    key_name = "${var.key_name}"
     user_data = <<-EOF
                 #!/bin/bash
                 apt-get install nginx apache2-utils -y
+                systemctl enable nginx
                 wget "https://s3-${var.region}.amazonaws.com/${aws_s3_bucket.my_bucket.id}/index.html" -P /var/www/html/
                 EOF
-
+    spot_price = "0.05"
 }
-output "public_ip" {
-  value = "${aws_instance.myinstance.public_ip}"
+
+resource "aws_autoscaling_group" "my_asg" {
+    name_prefix = "test-asg"
+    launch_configuration = "${aws_launch_configuration.my_lc.name}"
+    min_size = 1
+    max_size = 3
+    health_check_type = "EC2"
+    load_balancers = ["${aws_elb.test_lb.name}"]
+    vpc_zone_identifier = ["${var.private_subnets}"]
+    termination_policies = ["OldestLaunchConfiguration"]
+}
+
+resource "aws_autoscaling_policy" "asg_policy" {
+    name = "asg_cpu_policy"
+    policy_type = "TargetTrackingScaling"
+
+    target_tracking_configuration {
+      predefined_metric_specification {
+        predefined_metric_type = "ASGAverageCPUUtilization"
+    }
+    target_value = 40.0
+  }
+    autoscaling_group_name = "${aws_autoscaling_group.my_asg.name}"
+}
+
+output "Load Balancer Endpoint" {
+  value = "${aws_elb.test_lb.dns_name}"
 }
